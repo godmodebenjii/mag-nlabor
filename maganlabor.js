f980981b114361b2f35d2029c6d5a7ddad74d0f5
godmodebenjii <144019958+godmodebenjii@users.noreply.github.com>
2023-12-20 10:56:35 +0100

    Add files via upload

---
+++ maganlabor.js
@@ -0,0 +1,11 @@
+    function myFunction() {
+        var x = document.querySelector("nav");
+        if (x.className == "") 
+        {
+            x.className = "responsive";
+        } else {
+            x.className = "";
+        }
+    }
+
+    
